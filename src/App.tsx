8aae53f3284487dbf5359db5b5721cd0d04388dd
johannhsdev <johannenrique63@gmail.com>
2025-07-30 18:17:10 -0400

    Limpiar imports sin usar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
 const links = [
